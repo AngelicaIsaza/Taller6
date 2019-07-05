3323cfbf2a2208c3bf90297dc3510cac4b0a93c0
Angelica <angelys_afi@hotmail.com>
2019-07-04 20:53:09 -0500

    PruebaTallerHilo

---
+++ Taller6/test/HiloTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ANGELICA
+ */
+public class HiloTest {
+    
+    public HiloTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test(timeout = 2500)
+    public void hilotest(){
+        Hilo.hilolargo();
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divtest(){
+        Hilo.div(3,0);
+    }
+
+}
